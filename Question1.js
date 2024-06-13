ea54a9e3beab8d245cb968bf5c89a888c7387635
riteshjk <96072567+riteshjk@users.noreply.github.com>
2024-06-13 20:12:05 +0530

    added one more approach

--- Question1.js
+++ Question1.js
@@ -22,11 +22,27 @@ const pattern = (n) => {
     }
 }
 
+// second approach to solve this question
+
+// const pattern = (n) => {
+//     for (let i = 1; i <= n; i++) {
+//         let bag = "";
+//         // Appending spaces for alignment
+//         for (let k = n - i; k > 0; k--) {
+//             bag += " ";
+//         }
+//         // Appending numbers in descending order
+//         for (let j = i; j >= 1; j--) {
+//             bag += j;
+//         }
+//         console.log(bag);
+//     }
+// }
+
 pattern(5)
 
 /*
-Explanation: This is the only way to solve this question, write now its time complexity is O(n^2) and space complexity is O(1) 
-we cannot reduce it for pattern problem 
+Explanation: In first case time complexity is O(n^2) and space complexity is O(1) and in the second case time complexity is O(n^3) and space complexity is O(1)
 */
 
 
